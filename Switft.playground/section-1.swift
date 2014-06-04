70b70d49ac4a3be5a7796a1c61a683ca8b4466f0
Ricardo Vazquez <rvazquez@freshbooks.com>
2014-06-04 14:31:09 -0400

    Adding playground file

---
+++ Switft.playground/section-1.swift
@@ -0,0 +1,31 @@
+// Playground - noun: a place where people can play
+
+import Cocoa
+
+var str = "Hello, playground"
+
+var hello = "hello, world"
+
+let implicitDouble = 40.0
+
+let apples = 5
+let oranges = 3
+
+var question = "How many fruits do I have?"
+
+var fruits = apples + oranges
+
+var answer = "You have "
+
+let constant: Float = 4.0
+
+let label = "The width is "
+
+let width = 94
+
+let widthLabel = label + String(width) + "px."
+
+let appleSummary = "I have \(apples) apples with me."
+
+let fruitSummary = "I have \(apples + oranges) fruits with me."
+
